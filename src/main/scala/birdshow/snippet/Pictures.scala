d1d2ce1dfeecaf5e15499411fa5fdc9ff478a344
Dave Briccetti <daveb@davebsoft.com>
2009-09-26 15:20:56 -0700

    Use binding to allow moving more of the layout to the template

--- src/main/scala/birdshow/snippet/Pictures.scala
+++ src/main/scala/birdshow/snippet/Pictures.scala
@@ -1,15 +1,23 @@
 package birdshow.snippet
 
 import model.Flickr
+import net.liftweb.util.Helpers._
+import xml.{Node, Text, NodeSeq}
 
 class Pictures {
   val tag = Flickr.getTag
   val photos = Flickr.getPhotos(tag)
   
-  def list = <span><p>Pictures with tag “{tag}”</p> { photos.map(p => {
-    val picloc = "http://farm" + (p \ "@farm") + ".static.flickr.com/" + (p \ "@server") + 
-            "/" + (p \ "@id") + "_" + (p \ "@secret") + "_m.jpg"
-    <img style="margin-right: 1em" src={picloc}/>
-  })} </span>
-  
+  def render(content: NodeSeq): NodeSeq = 
+    bind("tagcol", content,
+      "tag" -> Text(tag),
+      "photos" -> photos.flatMap(photo => 
+        bind("item", chooseTemplate("photo", "list", content),
+          "img" -> <img src={url(photo)}/>, 
+          "title" -> (photo \ "@title")
+          )))
+                               
+  private def url(p: Node) = "http://farm" + (p \ "@farm") + ".static.flickr.com/" + 
+      (p \ "@server") + "/" + (p \ "@id") + "_" + (p \ "@secret") + "_m.jpg"
+
 }
