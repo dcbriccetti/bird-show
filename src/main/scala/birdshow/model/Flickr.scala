fb93e78e6c3b4870bd25ab5f464c7906da48c737
Dave Briccetti <daveb@davebsoft.com>
2009-12-03 15:14:59 -0800

    Add search feature

--- src/main/scala/birdshow/model/Flickr.scala
+++ src/main/scala/birdshow/model/Flickr.scala
@@ -2,25 +2,26 @@ package birdshow.model
 
 import java.net.{HttpURLConnection, URL}
 import xml.{NodeSeq, Node, XML}
+import birdshow.util.Loggable
 
-object Flickr {
+object Flickr extends Loggable {
   val userName = "Eleanor%20Briccetti"
   val masterCollectionName = "Selected"
 
   val urlPart1 = "http://api.flickr.com/services/rest/?method=flickr."
   val apiKey = "&api_key=979e4a1aa2eb498c845415e254e70f53"
     
-  val id: NodeSeq = getFromFlickr("people.findByUsername&username=" + userName) \ "user" \ "@nsid"
-  val collections: NodeSeq = getFromFlickr("collections.getTree&user_id=" + id) \\ "collection"
+  val userId: NodeSeq = getFromFlickr("people.findByUsername&username=" + userName) \ "user" \ "@nsid"
+  val collections: NodeSeq = getFromFlickr("collections.getTree&user_id=" + userId) \\ "collection"
   val setIds = ((collections filter(c => (c \ "@title") == "Selected")) \\ "set") map(_ \ "@id")
 
-  val sets: NodeSeq = (getFromFlickr("photosets.getList&user_id=" + id) \ "photosets" \ "photoset") filter(
+  val sets: NodeSeq = (getFromFlickr("photosets.getList&user_id=" + userId) \ "photosets" \ "photoset") filter(
           s => setIds.contains(s \\ "@id"))
 
-  def getPhotos(tag: String) = getFromFlickr("photos.search&user_id=" + id + "&tags=" + tag) \ "photos" \ "photo"
-  def getAllPhotos = getFromFlickr("photos.search&user_id=" + id + "&per_page=500") \ "photos" \ "photo"
+  def searchPhotos(searchText: String) = getFromFlickr("photos.search&user_id=" + userId + "&text=" + searchText) \ "photos" \ "photo"
+  def getAllPhotos = getFromFlickr("photos.search&user_id=" + userId + "&per_page=500") \ "photos" \ "photo"
   def getSetPhotos(setId: String) = {
-    val photos = getFromFlickr("photosets.getPhotos&user_id=" + id +
+    val photos = getFromFlickr("photosets.getPhotos&user_id=" + userId +
             "&photoset_id=" + setId + "&per_page=500") \ "photoset" \ "photo"
     println("Got " + photos.length + " photos")
     photos
@@ -33,9 +34,9 @@ object Flickr {
   private def getFromFlickr(urlBody: String) = XmlFetcher.get(urlPart1 + urlBody + apiKey)
 }
 
-object XmlFetcher {
+object XmlFetcher extends Loggable {
   def get(urlString: String): Node = {
-    println(urlString)
+    info(urlString)
     val conn = new URL(urlString).openConnection.asInstanceOf[HttpURLConnection]
     val statusCode = conn.getResponseCode
     if (statusCode != 200)
--- src/main/scala/birdshow/snippet/Pictures.scala
+++ src/main/scala/birdshow/snippet/Pictures.scala
@@ -4,31 +4,36 @@ import birdshow.model.Flickr
 import net.liftweb.util.Helpers._
 import xml.{Text, Node, NodeSeq}
 import net.liftweb.util.{Full}
-import net.liftweb.http.{S}
+import birdshow.util.Loggable
+import net.liftweb.http.{RequestVar, SHtml, S}
 
-class Pictures {
+class Pictures extends Loggable {
+  private object searchText extends RequestVar("")
+  
+  def search(content: NodeSeq): NodeSeq = {
+    def processSearch() {
+      debug("Searching for " + searchText.is)
+    }
+
+    bind("search", content,
+      "text" -> SHtml.text(searchText.is, searchText(_)),
+      "submit" -> SHtml.submit("Search", processSearch)
+      )
+  }
   
   def showGalleries(content: NodeSeq): NodeSeq = {
     
-    def bindGallery(content: NodeSeq, id: String): NodeSeq = {
+    def bindGalleryWithId(content: NodeSeq, id: String): NodeSeq = {
       bind("gal", content,
         "heading" -> Text(Flickr.getSets.find(s => (s \ "@id").text == id) match {
           case Some(photoSet) => (photoSet \ "title").text
           case _ => ""
         }),
         "showAll" -> <a href="?">Show gallery index</a>,
-        "galleries" -> group(Flickr.getSetPhotos(id)).flatMap(pGroup => 
-          bind("item", chooseTemplate("photo", "list", content),
-            "img1"   -> pImg(pGroup._1), 
-            "title1" -> pTitle(pGroup._1),
-            "img2"   -> pImg(pGroup._2), 
-            "title2" -> pTitle(pGroup._2),
-            "img3"   -> pImg(pGroup._3), 
-            "title3" -> pTitle(pGroup._3)
-            )))
+        "galleries" -> bindGroup(content, Flickr.getSetPhotos(id)))
     }
     
-    def bindGalleries(content: NodeSeq): NodeSeq = {
+    def bindAllGalleries(content: NodeSeq): NodeSeq = {
       bind("gal", content,
         "heading" -> "",
         "showAll" -> <span/>,
@@ -43,10 +48,22 @@ class Pictures {
             )))
     }
     
-    S.param("id") match {
-      case Full(id) => bindGallery(content, id)
-      case _ => bindGalleries(content)
+    def bindSearchResults(content: NodeSeq): NodeSeq = {
+      debug("bindGallery. searchText: " + searchText.is)
+      val xml: NodeSeq = Flickr.searchPhotos(searchText.is)
+      bind("gal", content,
+        "heading" -> Text("Results for " + searchText.is),
+        "showAll" -> <a href="?">Show gallery index</a>,
+        "galleries" -> bindGroup(content, xml))
     }
+    
+    if (searchText.is != "") 
+      bindSearchResults(content)
+    else
+      S.param("id") match {
+        case Full(id) => bindGalleryWithId(content, id)
+        case _ => bindAllGalleries(content)
+      }
   }
   
   def titles(content: NodeSeq): NodeSeq = <p>{Flickr.getAllPhotos.map(_ \ "@title").mkString(", ")}</p>
@@ -65,6 +82,16 @@ class Pictures {
     result
   }
   
+  private def bindGroup(content: NodeSeq, xml: NodeSeq) = group(xml).flatMap(pGroup => 
+    bind("item", chooseTemplate("photo", "list", content),
+      "img1"   -> pImg(pGroup._1), 
+      "title1" -> pTitle(pGroup._1),
+      "img2"   -> pImg(pGroup._2), 
+      "title2" -> pTitle(pGroup._2),
+      "img3"   -> pImg(pGroup._3), 
+      "title3" -> pTitle(pGroup._3)
+      ))
+
   private def pImg(photoSet: Option[Node]): NodeSeq = photoSet match {
     case Some(p) => <img src={Flickr.url(p, "id")}/>
     case None => <p/>
@@ -89,5 +116,5 @@ class Pictures {
   private def getSetTitles: List[Tuple2[String,String]] = {
     Flickr.getSets.map(s => ((s \ "@id").text, (s \ "title").text)).toList.sort(_._2 < _._2)
   }
-  
+
 }
