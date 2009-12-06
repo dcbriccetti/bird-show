9b147c584f255674db36d1929748ce9f07917ef5
Dave Briccetti <daveb@davebsoft.com>
2009-12-06 14:46:34 -0800

    Copy this over from TalkingPuffin. Later create a shared library?

---
+++ src/main/scala/birdshow/util/Parallelizer.scala
@@ -0,0 +1,19 @@
+// TODO    This is copied from TalkingPuffin. Don’t modify.
+
+package birdshow.util
+
+import java.util.concurrent.{ExecutorCompletionService, Callable, Executors}
+
+object Parallelizer {
+  /**
+   * Runs, in the number of threads requested, the function f, giving it each A of args, returning a List[T]
+   */
+  def run[T,A](numThreads: Int, args: Seq[A], f: (A) => T): List[T] = {
+    val pool = Executors.newFixedThreadPool(numThreads)
+    val completionService = new ExecutorCompletionService[T](pool)
+    args.foreach(arg => completionService.submit(new Callable[T] {def call = f(arg)}))
+    val result = args map(_ => completionService.take.get)
+    pool.shutdown
+    result.toList
+  }
+}
