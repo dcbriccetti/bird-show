d4cdc2df164bf109c33a6aad89ad7ba9193061be
Dave Briccetti <daveb@davebsoft.com>
2009-12-12 00:23:23 -0800

    Simplify, clean up.

--- src/main/scala/birdshow/snippet/Pictures.scala
+++ src/main/scala/birdshow/snippet/Pictures.scala
@@ -10,77 +10,61 @@ import birdshow.util.{Group, Loggable}
 
 class Pictures extends Loggable {
   private object searchText extends RequestVar("")
+  private type PicAndSizes = Tuple2[Node, PictureIdAndSizes]
   
-  def home(content: NodeSeq): NodeSeq = 
-    bind("home", content,
-      "randomUrl" -> <img src={Flickr.getRandomHomePhotoUrl}/>)
+  def home(content: NodeSeq) = bind("home", content,
+    "randomUrl" -> <img src={Flickr.getRandomHomePhotoUrl}/>)
   
-  def search(content: NodeSeq): NodeSeq = {
-    def processSearch() {
-      debug("Searching for " + searchText.is)
-    }
+  def search(content: NodeSeq) = bind("search", content,
+    "text" -> SHtml.text(searchText.is, searchText(_)),
+    "submit" -> SHtml.submit("Search", () => {}))
 
-    bind("search", content,
-      "text" -> SHtml.text(searchText.is, searchText(_)),
-      "submit" -> SHtml.submit("Search", processSearch)
-      )
-  }
-  
   def showShowPictures(content: NodeSeq): NodeSeq = {
-    def pImg(photoAndSize: Option[Tuple2[Node, PictureIdAndSizes]]): NodeSeq = photoAndSize match {
+    def pImg(photoAndSize: Option[PicAndSizes]): NodeSeq = photoAndSize match {
       case Some((photo, pictureIdAndSizes)) => <img src={pictureIdAndSizes.getSmallSizeUrl}/>
       case None => <span/>
     }
 
-    def bindGroup(content: NodeSeq, photosAndSizes: Flickr.PhotosSeq) = { 
-      Group.group(photosAndSizes).flatMap(pGroup => 
-      bind("item", chooseTemplate("photo", "list", content),
-        "img1"   -> pImg(pGroup._1), 
-        "title1" -> pTitle(pGroup._1),
-        "img2"   -> pImg(pGroup._2), 
-        "title2" -> pTitle(pGroup._2),
-        "img3"   -> pImg(pGroup._3), 
-        "title3" -> pTitle(pGroup._3)
-        ))
-    }
+    def bindGroup(content: NodeSeq, photosAndSizes: Flickr.PhotosSeq) =  
+      Group.group(photosAndSizes).flatMap(picGroup => 
+        bind("item", chooseTemplate("photo", "list", content),
+          "img1"   -> pImg  (picGroup._1), 
+          "title1" -> pTitle(picGroup._1),
+          "img2"   -> pImg  (picGroup._2), 
+          "title2" -> pTitle(picGroup._2),
+          "img3"   -> pImg  (picGroup._3), 
+          "title3" -> pTitle(picGroup._3)))
+    
     bind("gal", content,
       "galleries" -> bindGroup(content, Flickr.getSetPhotosAndSizes(Flickr.getUser.showSetId)))
   }
   
   def showGalleries(content: NodeSeq): NodeSeq = {
     
-    def bindAllGalleries(content: NodeSeq): NodeSeq = {
-      bind("gal", content,
-        "heading" -> "",
-        "showAll" -> <span/>,
-        "galleries" -> Group.group(Flickr.getSets).flatMap(psGroup => 
-          bind("item", chooseTemplate("photo", "list", content),
-            "img1"   -> psAnchor(psGroup._1), 
-            "title1" -> psTitle(psGroup._1),
-            "img2"   -> psAnchor(psGroup._2), 
-            "title2" -> psTitle(psGroup._2),
-            "img3"   -> psAnchor(psGroup._3), 
-            "title3" -> psTitle(psGroup._3)
-            )))
-    }
-    
-    def bindGalleryWithId(content: NodeSeq, id: String): NodeSeq = {
-      bind("gal", content,
+    def bindAllGalleries(content: NodeSeq) = bind("gal", content,
+      "heading" -> "",
+      "showAll" -> "",
+      "galleries" -> Group.group(Flickr.getSets).flatMap(setGroup => 
+        bind("item", chooseTemplate("photo", "list", content),
+          "img1"   -> psAnchor(setGroup._1), 
+          "title1" -> psTitle (setGroup._1),
+          "img2"   -> psAnchor(setGroup._2), 
+          "title2" -> psTitle (setGroup._2),
+          "img3"   -> psAnchor(setGroup._3), 
+          "title3" -> psTitle (setGroup._3))))
+
+    def bindGalleryWithId(content: NodeSeq, id: String) = bind("gal", content,
         "heading" -> Text(Flickr.getSets.find(s => (s \ "@id").text == id) match {
           case Some(photoSet) => (photoSet \ "title").text
           case _ => ""
         }),
         "showAll" -> <a href="?">Show gallery index</a>,
         "galleries" -> bindGroup(content, Flickr.getSetPhotosAndSizes(id)))
-    }
     
-    def bindSearchResults(content: NodeSeq): NodeSeq = {
-      debug("bindGallery. searchText: " + searchText.is)
-      bind("gal", content,
+    def bindSearchResults(content: NodeSeq) = bind("gal", content,
         "heading" -> Text("Results for " + searchText.is),
         "showAll" -> <a href="?">Show gallery index</a>,
         "galleries" -> bindGroup(content, Flickr.searchPhotos(searchText.is)))
-    }
     
     if (searchText.is != "") 
       bindSearchResults(content)
@@ -95,18 +79,17 @@ class Pictures extends Loggable {
   
   private def bindGroup(content: NodeSeq, photosAndSizes: Flickr.PhotosSeq) = 
     Group.group(photosAndSizes).flatMap(pGroup => 
-    bind("item", chooseTemplate("photo", "list", content),
-      "img1"   -> pImg(pGroup._1), 
-      "title1" -> pTitle(pGroup._1),
-      "img2"   -> pImg(pGroup._2), 
-      "title2" -> pTitle(pGroup._2),
-      "img3"   -> pImg(pGroup._3), 
-      "title3" -> pTitle(pGroup._3)
-      ))
+      bind("item", chooseTemplate("photo", "list", content),
+        "img1"   -> pImg  (pGroup._1), 
+        "title1" -> pTitle(pGroup._1),
+        "img2"   -> pImg  (pGroup._2), 
+        "title2" -> pTitle(pGroup._2),
+        "img3"   -> pImg  (pGroup._3), 
+        "title3" -> pTitle(pGroup._3)))
 
   private case class PrioritizedSource(val priority: Int, val source: String)
   
-  private def pImg(photoAndSize: Option[Tuple2[Node, PictureIdAndSizes]]): NodeSeq = photoAndSize match {
+  private def pImg(photoAndSize: Option[PicAndSizes]): NodeSeq = photoAndSize match {
     case Some((photo, pictureIdAndSizes)) =>
       <a href="#">
         <img onclick={"BIRDSHOW.showBig('" + pictureIdAndSizes.getPreferredSizeUrl + "'); return false;"} 
@@ -115,7 +98,7 @@ class Pictures extends Loggable {
     case None => <p/>
   }
   
-  private def pTitle(photoSet: Option[Tuple2[Node, PictureIdAndSizes]]): String = photoSet match {
+  private def pTitle(photoSet: Option[PicAndSizes]): String = photoSet match {
     case Some(ps) => (ps._1 \ "@title").text
     case None => ""
   }
