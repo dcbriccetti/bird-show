021a57c4e79d2aa3c5f0ee67b8151e438c814c50
Dave Briccetti <daveb@davebsoft.com>
2009-12-12 21:27:20 -0800

    Improve JavaScript

--- src/main/webapp/js/birdshow.js
+++ src/main/webapp/js/birdshow.js
@@ -1,19 +1,20 @@
 var BIRDSHOW = {
+
     urlShowing: '',
+
     showBig: function(url) {
         if (BIRDSHOW.urlShowing === url) {
             BIRDSHOW.hideBig();
         } else {
             $('#bigImage').attr('src', url);
             $('#bigImageDiv').show('slow');
-            BIRDSHOW.urlShowing = url
+            BIRDSHOW.urlShowing = url;
         }
     },
+
     hideBig: function() {
         $('#bigImageDiv').hide('slow');
-        BIRDSHOW.urlShowing = ''
+        BIRDSHOW.urlShowing = '';
     }
 };
-$(document).ready(function() {
-    $('#bigImageDiv').hide();
-})
+
