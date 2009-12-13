f1a2f8ec8e258acc4af66c93ca25c566881869b2
Indrajit Raychaudhuri <irc@indrajit.com>
2009-12-14 00:50:02 +0530

    - Upgrade to Lift 1.1-SNAPSHOT and Scala 2.7.7 - Cosmetic cleanup Boot.scala

--- src/main/scala/birdshow/snippet/Pictures.scala
+++ src/main/scala/birdshow/snippet/Pictures.scala
@@ -2,24 +2,24 @@ package birdshow.snippet
 
 import net.liftweb.util.Helpers._
 import xml.{Text, NodeSeq}
-import net.liftweb.util.{Full}
+import net.liftweb.common.{Full}
 import net.liftweb.http.{RequestVar, SHtml, S}
 import birdshow.model.Flickr
 import birdshow.util.{Loggable}
 
 class Pictures extends Loggable with PhotoRows {
   private object searchText extends RequestVar("")
-  
+
   def search(content: NodeSeq) = bind("search", content,
     "text" -> SHtml.text(searchText.is, searchText(_)),
     "submit" -> SHtml.submit("Search", () => {}))
 
   def showGalleries(content: NodeSeq): NodeSeq = {
-    
+
     def bindAllGalleries(content: NodeSeq) = bind("gal", content,
       "heading" -> "",
       "showAll" -> "",
-      "photoRows" -> bindPhotoRows(content, Flickr.getSets, psAnchor, psTitle)) 
+      "photoRows" -> bindPhotoRows(content, Flickr.getSets, psAnchor, psTitle))
 
     def bindGalleryWithId(content: NodeSeq, id: String) = bind("gal", content,
         "heading" -> Text(Flickr.getSets.find(_.id == id) match {
@@ -28,13 +28,13 @@ class Pictures extends Loggable with PhotoRows {
         }),
         "showAll" -> <a href="?">Show gallery index</a>,
         "photoRows" -> bindPhotoRows(content, Flickr.getSetPhotosAndSizes(id), pImg, pTitle))
-    
+
     def bindSearchResults(content: NodeSeq) = bind("gal", content,
         "heading" -> Text("Results for " + searchText.is),
         "showAll" -> <a href="?">Show gallery index</a>,
         "photoRows" -> bindPhotoRows(content, Flickr.searchPhotos(searchText.is), pImg, pTitle))
-    
-    if (searchText.is != "") 
+
+    if (searchText.is != "")
       bindSearchResults(content)
     else
       S.param("id") match {
--- src/main/scala/bootstrap/liftweb/Boot.scala
+++ src/main/scala/bootstrap/liftweb/Boot.scala
@@ -1,43 +1,45 @@
 package bootstrap.liftweb
 
 import _root_.net.liftweb.util._
+import _root_.net.liftweb.common._
 import _root_.net.liftweb.http._
+import _root_.net.liftweb.http.provider._
 import _root_.net.liftweb.sitemap._
 import _root_.net.liftweb.sitemap.Loc._
-import Helpers._
 import _root_.birdshow.model._
-import _root_.javax.servlet.http.{HttpServletRequest}
 
 /**
-  * A class that's instantiated early and run.  It allows the application
-  * to modify lift's environment
-  */
+ * A class that's instantiated early and run.  It allows the application
+ * to modify lift's environment
+ */
 class Boot {
   def boot {
     // where to search snippet
     LiftRules.addToPackages("birdshow")
 
     // Build SiteMap
-    LiftRules.setSiteMap(SiteMap(
-      Menu(Loc("Home",      List("index"    ), "Home"     )),
-      Menu(Loc("Biography", List("bio"      ), "Biography")),
-      Menu(Loc("Galleries", List("galleries"), "Galleries")),
-      Menu(Loc("Shows",     List("shows"    ), "Shows"    )),
-      Menu(Loc("Prints",    List("prints"   ), "Prints"   ))
-      ))
+    val entries =
+      Menu(Loc("Home",      List("index"    ), "Home"     )) ::
+      Menu(Loc("Biography", List("bio"      ), "Biography")) ::
+      Menu(Loc("Galleries", List("galleries"), "Galleries")) ::
+      Menu(Loc("Shows",     List("shows"    ), "Shows"    )) ::
+      Menu(Loc("Prints",    List("prints"   ), "Prints"   )) :: Nil
+    LiftRules.setSiteMap(SiteMap(entries: _*))
+
     LiftRules.early.append(makeUtf8)
     LiftRules.enableLiftGC = false
-    
-    Flickr.addUser(LiftRules.context.getInitParameter("flickrUserName"),
-      LiftRules.context.getInitParameter("flickrCollection"),
-      LiftRules.context.getInitParameter("flickrHomeSet"),
-      LiftRules.context.getInitParameter("flickrShowSet"))
+
+    Flickr.addUser(
+      LiftRules.context.initParam("flickrUserName") openOr "",
+      LiftRules.context.initParam("flickrCollection") openOr "",
+      LiftRules.context.initParam("flickrHomeSet") openOr "",
+      LiftRules.context.initParam("flickrShowSet") openOr "")
   }
 
   /**
    * Force the request to be UTF-8
    */
-  private def makeUtf8(req: HttpServletRequest) {
+  private def makeUtf8(req: HTTPRequest) {
     req.setCharacterEncoding("UTF-8")
   }
 
