ac43f1da47be1f1b32bb81c9563d7ffc4700b425
Dave Briccetti <daveb@davebsoft.com>
2010-08-21 13:54:42 -0700

    Accommodate new Flickr picture sizes

--- src/main/scala/birdshow/model/flickr/PictureIdAndSizes.scala
+++ src/main/scala/birdshow/model/flickr/PictureIdAndSizes.scala
@@ -6,18 +6,21 @@ case class PictureSize(val sizeName: String, val url: String)
 
 case class PictureIdAndSizes(val id: String, val sizes: Seq[PictureSize]) {
   private val SmallSizeName = "Small" // Avoid duplicating literal
-  
+
   private val sizeSelectionOrder = Map(
     "Large" -> 1,
-    "Medium" -> 2,
-    SmallSizeName -> 3,
-    "Original" -> 4,
-    "Thumbnail" -> 5,
-    "Square" -> 6
+    "Medium 640" -> 2,
+    "Medium 500" -> 3,
+    "Medium" -> 4, // Still exists?
+    SmallSizeName -> 5,
+    "Original" -> 6,
+    "Thumbnail" -> 7,
+    "Square" -> 8
     )
 
   def getPreferredSizeUrl: String = {
-    sizes.toList.sort((a,b) => sizeSelectionOrder(a.sizeName) < sizeSelectionOrder(b.sizeName))(0).url
+    def getOrderCode(key: String) = sizeSelectionOrder.getOrElse(key, 100)
+    sizes.toList.sort((a,b) => getOrderCode(a.sizeName) < getOrderCode(b.sizeName))(0).url
   }
 
   def getSmallSizeUrl: String = {
